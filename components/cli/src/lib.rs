dfcada81032c81d7e7ecc2715c62d3f78dfe5357
xavier <xavier2code@gmail.com>
2025-02-08 16:56:50 +0800

    fix(rust): update clap enum

--- components/cli/src/lib.rs
+++ components/cli/src/lib.rs
@@ -1,10 +1,21 @@
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
     show: Option<Commands>,
+
+    #[arg(value_enum)]
+    mode: Mode,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Mode {
+    Author,
+    Card,
+    Nano,
+    Replace,
 }
 
 #[derive(Subcommand, Debug)]
@@ -20,6 +31,21 @@ pub fn run() {
 
     if let Some(directory) = cli.show {
         println!("{:?}", directory);
+    }
+
+    match cli.mode {
+        Mode::Author => {
+            println!("Author mode selected");
+        }
+        Mode::Card => {
+            println!("Card mode selected");
+        }
+        Mode::Nano => {
+            println!("Nano mode selected");
+        }
+        Mode::Replace => {
+            println!("Replace mode selected");
+        }
     }
 }
 
